d82df7aca9d9b3c578621e3ad74f5b01b2ec674b
leloeduk <leloeduk2025@gmail.com>
2025-09-02 00:18:39 +0100

    ajout de multiblocprovider dans main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:samyauto/features/dashboard/presentation/pages/dashboard_page.dart';
+import 'package:samyauto/features/factures/presentation/bloc/facture_bloc.dart';
 import 'core/hive/hive_setup.dart';
+import 'features/factures/data/facture_repo.dart';
+import 'features/services/data/service_repo.dart';
+import 'features/services/presentation/bloc/service_bloc.dart';
+import 'features/services/presentation/bloc/service_event.dart';
+import 'features/services/presentation/pages/service_form_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,11 +21,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Samy auto Offline',
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
-      home: const DashboardPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => ServiceBloc(ServiceRepository())..add(LoadServices()),
+          child: ServiceFormPage(),
+        ),
+        BlocProvider(create: (context) => FactureBloc(FactureRepository())),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Samy auto Offline',
+        theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+        home: const DashboardPage(),
+      ),
     );
   }
 }
